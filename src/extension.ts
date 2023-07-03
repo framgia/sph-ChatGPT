67af099895eea341298e340156cc36e615d13829
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2023-07-03 15:16:38 +0800

    PJ_SIM_CHATGPT_2023-17 Translation: Add input field for API Key (#4)

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,23 @@ import * as vscode from 'vscode';
 import { SidebarProvider } from './providers/SidebarProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-  const sidebarProvider = new SidebarProvider(context.extensionUri);
+  const sidebarProvider = new SidebarProvider(context);
   const sidebarDisposable = vscode.window.registerWebviewViewProvider(
     'sim-chatgpt-translator-sidebar',
     sidebarProvider
   );
   context.subscriptions.push(sidebarDisposable);
+
+  const apiKeyCommandDisposable = vscode.commands.registerCommand(
+    'sim-chatgpt-translator.runApiKey',
+    () => {
+      sidebarProvider._view?.webview.postMessage({
+        type: 'isApiKeyClicked',
+        value: true,
+      });
+    }
+  );
+  context.subscriptions.push(apiKeyCommandDisposable);
 }
 
 // This method is called when your extension is deactivated
