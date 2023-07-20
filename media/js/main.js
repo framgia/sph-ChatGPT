e9b520f803b20491deec8c33186ef3aa80c2e56a
Brucenigel <brucenige.vilo@sun-asterisk.com>
2023-07-20 16:30:31 +0800

    PJ_SIM_CHATGPT_2023-33 Translation: Add dialog box on chatGPT output

--- media/js/main.js
+++ media/js/main.js
@@ -42,16 +42,38 @@
         textarea.value = message.value + textarea.value;
         textarea.style.height = '';
         textarea.style.height = Math.min(textarea.scrollHeight, 500) + 'px';
+
+        localStorage.setItem('selectedData', textarea.value);
         handleLoading(true);
         vscode.postMessage({ type: 'queryChatGPT', value: textarea.value });
         break;
       }
       case 'onChatGPTResponse': {
         handleLoading(false);
-        const container = document.getElementById('response-container');
-        container.value = message.value;
-        container.style.height = '';
-        container.style.height = container.scrollHeight + 'px';
+        const existingArrayString = localStorage.getItem('arrayGptOutput');
+        const selectedArrayString = localStorage.getItem('selectedArray');
+        let existingArray = [];
+        let selectedArray = [];
+
+        if (existingArrayString && selectedArray) {
+          existingArray = JSON.parse(existingArrayString);
+          selectedArray = JSON.parse(selectedArrayString);
+        }
+
+        existingArray.push(message.value);
+        selectedArray.push(localStorage.getItem('selectedData'));
+        const clearInput = document.getElementById('input-query');
+        clearInput.style.height = '';
+        clearInput.value = '';
+        localStorage.setItem('selectedData', clearInput.value);
+        clearInput.ariaPlaceholder = 'Highlight code snippet to ask GPT...';
+
+        const updatedArrayString = JSON.stringify(existingArray);
+        const updatedSelectedArrayString = JSON.stringify(selectedArray);
+
+        localStorage.setItem('arrayGptOutput', updatedArrayString);
+        localStorage.setItem('selectedArray', updatedSelectedArrayString);
+        displayRecent();
         break;
       }
     }
@@ -92,7 +114,7 @@
   function handleLoading(isLoading) {
     let searchOutput = document.getElementById('response-container');
     let loading = document.getElementById('gear-container');
-    let cancel = document.getElementById('cancel');
+    let cancel = document.getElementById('cancel-request');
     if (isLoading) {
       searchOutput.classList.add('hidden');
       loading.classList.remove('hidden');
@@ -103,8 +125,58 @@
       cancel.classList.add('hidden');
     }
   }
-  const cancelLoading = document.getElementById('cancel');
+  const cancelLoading = document.getElementById('cancel-request');
   cancelLoading.onclick = () => {
     vscode.postMessage({ type: 'cancelQuery', value: null });
   };
+
+  function displayRecent() {
+    if (localStorage.getItem('arrayGptOutput')) {
+      const textarea = document.querySelector('.card-indicator');
+      textarea.classList.add('hidden');
+      const container = document.querySelector('.dialog-box');
+      const data = JSON.parse(localStorage.getItem('arrayGptOutput'));
+      const selectedData = JSON.parse(localStorage.getItem('selectedArray'));
+
+      if (data.length) {
+        for (let i = 0; i < data.length; i++) {
+          const existingData = Array.from(
+            container.querySelectorAll('textarea')
+          ).map((textarea) => textarea.value);
+          if (!existingData.includes(data[i])) {
+            const card = document.createElement('div');
+            card.className = 'card border-right';
+
+            const textareaClone = document.createElement('textarea');
+            textareaClone.value = data[i];
+            textareaClone.readOnly = true;
+            textareaClone.style.height = 'auto';
+            textareaClone.className = 'response-container w-full p-2';
+
+            card.appendChild(textareaClone);
+            container.insertBefore(card, container.firstChild);
+            textareaClone.style.height =
+              Math.min(textareaClone.scrollHeight, 500) + 'px';
+
+            const selectedCard = document.createElement('div');
+            selectedCard.className = 'card border-left';
+
+            const selectedClone = document.createElement('textarea');
+            selectedClone.value = selectedData[i];
+            selectedClone.readOnly = true;
+            selectedClone.style.height = 'auto';
+            selectedClone.className = 'response-container w-full p-2';
+
+            selectedCard.appendChild(selectedClone);
+            container.insertBefore(selectedCard, container.firstChild);
+            selectedClone.style.height =
+              Math.min(selectedClone.scrollHeight, 500) + 'px';
+          }
+        }
+      } else {
+        const textarea = document.querySelector('.card-indicator');
+        textarea.classList.remove('hidden');
+      }
+    }
+  }
 })();
